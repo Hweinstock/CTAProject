c87811f16ee100f52f26a7e308b4bf6500d2cda1
Hweinstock <42325418+Hweinstock@users.noreply.github.com>
2023-03-06 16:59:59 -0500

    messing around with data

--- source/playground.py
+++ source/playground.py
@@ -1,4 +1,32 @@
 #!/Users/hweinstock/Documents/GitHub/CTAProject/venv/bin/python
+import pandas as pd 
+
+from typing import List
+
+class HeadlineData:
+
+    def __init__(self, csv_filepath: str):
+        self.filepath = csv_filepath
+        self.df = pd.read_csv(csv_filepath, index_col=0)
+    
+    def get_stock_list(self) -> pd.Series:
+        """
+        Returns:
+            pd.Series: Stock symbols in order of how frequently they occur in the data. 
+        """
+        return self.df['stock'].value_counts()
+    
+    def filter_by_stocks(self, stocks: List[str]) -> pd.DataFrame:
+        """
+        Args:
+            stocks (List[str]): filter
+
+        Returns:
+            pd.DataFrame: self.df filtered to rows with stocks in stock filter input. 
+        """
+        return self.df[self.df.stock.isin(stocks)]
 
 if __name__ == '__main__':
-    print('this file is working')
+    processed_data = HeadlineData('../data/set1/analyst_ratings_processed.csv')
+    tech_stocks = processed_data.filter_by_stocks(['AAPL', 'AMZN','GOOGL', 'MSFT', 'META'])
+    print(tech_stocks['stock'].value_counts())
