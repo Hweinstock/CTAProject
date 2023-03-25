06cc6c46d2f8258511fed93f20e785533759e772
Hweinstock <42325418+Hweinstock@users.noreply.github.com>
2023-03-25 14:47:39 -0400

    refactor

--- model/playground.py
+++ model/playground.py
@@ -3,6 +3,7 @@ from torch.utils.data import Dataset, DataLoader
 import torch
 import pandas as pd 
 from tqdm import tqdm
+from typing import Tuple
 
 # https://colab.research.google.com/github/DhavalTaunk08/NLP_scripts/blob/master/sentiment_analysis_using_roberta.ipynb
 
@@ -14,12 +15,12 @@ LEARNING_RATE = 1e-05
 from torch import cuda
 
 device = 'cuda' if cuda.is_available() else 'cpu'
-
 tokenizer = RobertaTokenizer.from_pretrained('roberta-base', truncation=True, do_lower_case=True)
-train_data_path = '../processed_stock_data/AAPL-data.csv'
-train_data = pd.read_csv(train_data_path)
 
-new_df = train_data[["title", "label"]]
+def get_train_data(train_data_path):
+    train_data = pd.read_csv(train_data_path)
+    new_df = train_data[["title", "label"]]
+    return new_df
 
 class HeadlineData(Dataset):
     def __init__(self, dataframe, tokenizer, max_len):
@@ -76,128 +77,152 @@ class RobertaClass(torch.nn.Module):
         output = self.classifier(pooler)
         return output
 
-def calculate_accuracy(preds, targets):
-    n_correct = (preds == targets).sum().item()
-    return n_correct
+class StockPredictionModel:
 
-def train(epoch):
-    tr_loss = 0
-    n_correct = 0 
-    nb_tr_steps = 0
-    nb_tr_examples = 0
-    model.train()
-    for _, data in tqdm(enumerate(training_loader, 0)):
-        ids = data['ids'].to(device, dtype=torch.long)
-        mask = data['mask'].to(device, dtype=torch.long)
-        token_type_ids = data['token_type_ids'].to(device, dtype=torch.long)
-        targets = data['targets'].to(device, dtype = torch.long)
+    def __init__(self, model: torch.nn.Module, loss_function, optimizer, data_source: pd.DataFrame):
+        self.model = model 
+        self.loss_function = loss_function 
+        self.optimizer = optimizer
 
-        outputs = model(ids, mask, token_type_ids)
-        loss = loss_function(outputs, targets)
-        tr_loss += loss.item()
-        big_val, big_idx = torch.max(outputs.data, dim=1)
-        n_correct += calculate_accuracy(big_idx, targets)
-
-        nb_tr_steps += 1 
-        nb_tr_examples += targets.size(0)
-
-        if _%5000==0:
-            loss_step = tr_loss/nb_tr_steps 
-            accu_step = (n_correct*100)/nb_tr_examples 
-            print(f"Training Loss per 5000 steps:  {loss_step}")
-            print(f"Training Accuracy per 5000 steps: {accu_step}")
+        self.training_loader, self.testing_loader = self.initialize_dataloaders(data_source)
         
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
     
-    print(f'The Total Accuracy for Epoch {epoch}: {(n_correct*100)/nb_tr_examples}')
-    epoch_loss = tr_loss/nb_tr_steps 
-    epoch_accu = (n_correct*100)/nb_tr_examples
-    print(f"Training Loss Epoch: {epoch_loss}")
-    print(f"Training accuracy epoch: {epoch_accu}")
+    def initialize_dataloaders(self, data_source: pd.DataFrame) -> Tuple[DataLoader]:
+        train_size = 0.8
+        train_data = data_source.sample(frac=train_size, random_state=200)
+        test_data = data_source.drop(train_data.index).reset_index(drop=True)
+        train_data = train_data.reset_index(drop=True)
 
-    return 
+        print(f"Full Dataset: {data_source.shape}")
+        print(f"Train Dataset: {train_data.shape}")
+        print(f"Test Dataset: {test_data.shape}")
 
-def valid(model, testing_loader):
-    model.eval()
-    n_correct = 0
-    n_wrong = 0
-    total = 0
-    tr_loss = 0
-    nb_tr_steps = 0 
-    nb_tr_examples= 0 
-    with torch.no_grad():
-        for _, data in tqdm(enumerate(testing_loader, 0)):
-            ids = data['ids'].to(device, dtype = torch.long)
-            mask = data['mask'].to(device, dtype = torch.long)
+        train_params = {
+            'batch_size': TRAIN_BATCH_SIZE, 
+            'shuffle': True, 
+            'num_workers': 0
+        }
+
+        test_params = {
+            'batch_size': VALID_BATCH_SIZE, 
+            'shuffle': True, 
+            'num_workers': 0
+        }
+
+        training_set = HeadlineData(train_data, tokenizer, MAX_LEN)
+        testing_set = HeadlineData(test_data, tokenizer, MAX_LEN)
+
+        training_loader = DataLoader(training_set, **train_params)
+        testing_loader = DataLoader(testing_set, **test_params)
+
+        return training_loader, testing_loader
+
+    def train(self, epoch):
+        tr_loss = 0
+        n_correct = 0 
+        nb_tr_steps = 0
+        nb_tr_examples = 0
+        self.model.train()
+        for _, data in tqdm(enumerate(self.training_loader, 0)):
+            ids = data['ids'].to(device, dtype=torch.long)
+            mask = data['mask'].to(device, dtype=torch.long)
             token_type_ids = data['token_type_ids'].to(device, dtype=torch.long)
-            targets = data['targets'].to(device, dtype=torch.long)
-            outputs = model(ids, mask, token_type_ids).squeeze()
-            loss = loss_function(outputs, targets)
+            targets = data['targets'].to(device, dtype = torch.long)
+
+            outputs = self.model(ids, mask, token_type_ids)
+            loss = self.loss_function(outputs, targets)
             tr_loss += loss.item()
             big_val, big_idx = torch.max(outputs.data, dim=1)
             n_correct += calculate_accuracy(big_idx, targets)
 
-            nb_tr_steps += 1
+            nb_tr_steps += 1 
             nb_tr_examples += targets.size(0)
 
-            if _% 5000 == 0:
-                loss_step = tr_loss / nb_tr_steps 
-                accu_step = (n_correct*100) / nb_tr_examples 
-                print(f"Validation Loss per 100 steps: {loss_step}")
-                print(f"Validation Accuracy per 100 steps: {accu_step}")
-    epoch_loss = tr_loss / nb_tr_steps 
-    epoch_accu = (n_correct*100) / nb_tr_examples 
-    print(f"Validation Loss Epoch: {epoch_loss}")
-    print(f"Validation Accuracy Epoch: {epoch_accu}")
+            if _%5000==0:
+                loss_step = tr_loss/nb_tr_steps 
+                accu_step = (n_correct*100)/nb_tr_examples 
+                print(f"Training Loss per 5000 steps:  {loss_step}")
+                print(f"Training Accuracy per 5000 steps: {accu_step}")
+            
+            self.optimizer.zero_grad()
+            loss.backward()
+            self.optimizer.step()
+        
+        print(f'The Total Accuracy for Epoch {epoch}: {(n_correct*100)/nb_tr_examples}')
+        epoch_loss = tr_loss/nb_tr_steps 
+        epoch_accu = (n_correct*100)/nb_tr_examples
+        print(f"Training Loss Epoch: {epoch_loss}")
+        print(f"Training accuracy epoch: {epoch_accu}")
 
-    return epoch_accu
+        return 
 
-train_size = 0.8
-train_data = new_df.sample(frac=train_size, random_state=200)
-test_data = new_df.drop(train_data.index).reset_index(drop=True)
-train_data = train_data.reset_index(drop=True)
+    def valid(self):
+        self.model.eval()
+        n_correct = 0
+        n_wrong = 0
+        total = 0
+        tr_loss = 0
+        nb_tr_steps = 0 
+        nb_tr_examples= 0 
+        with torch.no_grad():
+            for _, data in tqdm(enumerate(self.testing_loader, 0)):
+                ids = data['ids'].to(device, dtype = torch.long)
+                mask = data['mask'].to(device, dtype = torch.long)
+                token_type_ids = data['token_type_ids'].to(device, dtype=torch.long)
+                targets = data['targets'].to(device, dtype=torch.long)
+                outputs = self.model(ids, mask, token_type_ids).squeeze()
+                loss = self.loss_function(outputs, targets)
+                tr_loss += loss.item()
+                big_val, big_idx = torch.max(outputs.data, dim=1)
+                n_correct += calculate_accuracy(big_idx, targets)
 
-print(f"Full Dataset: {new_df.shape}")
-print(f"Train Dataset: {train_data.shape}")
-print(f"Test Dataset: {test_data.shape}")
+                nb_tr_steps += 1
+                nb_tr_examples += targets.size(0)
 
-train_params = {
-    'batch_size': TRAIN_BATCH_SIZE, 
-    'shuffle': True, 
-    'num_workers': 0
-}
+                if _% 5000 == 0:
+                    loss_step = tr_loss / nb_tr_steps 
+                    accu_step = (n_correct*100) / nb_tr_examples 
+                    print(f"Validation Loss per 100 steps: {loss_step}")
+                    print(f"Validation Accuracy per 100 steps: {accu_step}")
+        epoch_loss = tr_loss / nb_tr_steps 
+        epoch_accu = (n_correct*100) / nb_tr_examples 
+        print(f"Validation Loss Epoch: {epoch_loss}")
+        print(f"Validation Accuracy Epoch: {epoch_accu}")
 
-test_params = {
-    'batch_size': VALID_BATCH_SIZE, 
-    'shuffle': True, 
-    'num_workers': 0
-}
+        return epoch_accu
+    
+    def save_model(self, model_path: str, vocab_path: str):
 
-training_set = HeadlineData(train_data, tokenizer, MAX_LEN)
-testing_set = HeadlineData(test_data, tokenizer, MAX_LEN)
+        torch.save(self.model, model_path)
+        tokenizer.save_vocabulary(vocab_path)
 
-training_loader = DataLoader(training_set, **train_params)
-testing_loader = DataLoader(testing_set, **test_params)
+        print('All files saved')
+    
+def calculate_accuracy(preds, targets):
+    n_correct = (preds == targets).sum().item()
+    return n_correct
 
-model = RobertaClass() 
-model.to(device)
-loss_function = torch.nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(params = model.parameters(), lr = LEARNING_RATE)
+def main():
 
-EPOCHS = 1
-for epoch in range(EPOCHS):
-    train(epoch)
+    model = RobertaClass() 
+    model.to(device)
 
-acc = valid(model, testing_loader)
-print("Accuracy on test data = %0.2f%%" % acc)
+    loss_function = torch.nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(params = model.parameters(), lr = LEARNING_RATE)
 
-output_model_file = 'pytorch_roberta_sentiment.bin'
-output_vocab_file = './'
+    train_data_path = '../processed_stock_data/AAPL-data.csv'
+    df = get_train_data(train_data_path)
+    SPModel = StockPredictionModel(model, loss_function, optimizer, df)
+    EPOCHS = 1
+    for epoch in range(EPOCHS):
+        SPModel.train(epoch)
 
-model_to_save = model
-torch.save(model_to_save, output_model_file)
-tokenizer.save_vocabulary(output_vocab_file)
+    acc = SPModel.valid(model, SPModel.testing_loader)
+    print("Accuracy on test data = %0.2f%%" % acc)
 
-print('All files saved')
+    output_model_file = 'pytorch_roberta_sentiment.bin'
+    output_vocab_file = './'
+    SPModel.save_model(output_model_file, output_vocab_file)
+
+if __name__ == '__main__':
+    main()
