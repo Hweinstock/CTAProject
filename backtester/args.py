6275e35aa5b95f38e3042d4929e6971e452f3c1c
Hweinstock <42325418+Hweinstock@users.noreply.github.com>
2023-04-21 12:55:11 -0400

    slightly more cleanup

--- backtester/args.py
+++ backtester/args.py
@@ -1,11 +1,5 @@
 import argparse
 
-def add_output_dir_arguments(parser: argparse._ArgumentGroup, default: str) -> None:
-
-    parser.add_argument("-o", "--output_dir", type=str, 
-                                     help="path to directory where final csvs should be placed. ",
-                                     default=default)
-
 def add_test_predictions_arguments(parser: argparse.ArgumentParser) -> None:
     test_predictions_arguments = parser.add_argument_group('test_predictions_arguments')
 
@@ -17,8 +11,6 @@ def add_test_predictions_arguments(parser: argparse.ArgumentParser) -> None:
     test_predictions_arguments.add_argument("-o", "--output", type=str, 
                                      help="name of file to export both .csv strategy summary and .png plot",
                                      default='newresults')
-    
-    add_output_dir_arguments(parser, 'data/prediction_data')
 
 def get_test_predictions_arguments() -> argparse.Namespace:
     p = argparse.ArgumentParser(description=__doc__,
